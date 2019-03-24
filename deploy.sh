2ac90833de3b029c168c04270d07055216ffdd99
Matthieu TIRELLI <matthieu.tirelli@gmail.com>
2019-03-24 15:08:58 +0100

    Fixing tests

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,22 @@
-#!/bin/bash
+# SETTING UP KEY
 
-# TAKING THE PKEY FROM ENV AND SAVE IN TMP
-
-eval $(ssh-agent)
 echo $PRIVATE_SSH_KEY | base64 --decode > /tmp/.ssh_private
-echo "Host *\n\tUseKeychain yes\n\tAddKeysToAgent yes" > ~/.ssh/config
 chmod 600 /tmp/.ssh_private
 
-# CLONE GIT REPO
-rm -rf .git
+ssh-add /tmp/.ssh_private
 
-ssh-agent bash -c 'ssh-add /tmp/.ssh_private; git clone git@github.com:hostersecurity/hostersecurity.github.io.git'
+git clone git@github.com:hostersecurity/hostersecurity.github.io.git
+
+mkdir -p hostersecurity.github.io/ssllabs_test/
+mkdir -p hostersecurity.github.io/wpscan_test/
+
+cp ssllabs_test/* hostersecurity.github.io/ssllabs_test/
+cp wpscan_test/* hostersecurity.github.io/wpscan_test/
+
 cd hostersecurity.github.io
-
-mkdir -p wpscan_test ssllabs_test
-
-cp ../wpscan_test/* wpscan_test
-cp ../ssllabs_test/* ssllabs_test
-
-git add wpscan_test/*
-git add ssllabs_test/*
-
 git checkout -b testsresults/$TRAVIS_BUILD_NUMBER
-git commit -m "Autotests results $TRAVIS_BUILD_NUMBER"
+git add ssllabs_test/* wpscan_test/*
+git commit -m "Updated test results for $TRAVIS_BUILD_NBR"
 git config user.email "travis@hostersecurity.tld"
 git config user.name "Auto-test"
-git remote set-url origin git@github.com:hostersecurity/hostersecurity.github.io.git
-
-ssh-agent bash -c 'ssh-add /tmp/.ssh_private; git push origin testsresults/$TRAVIS_BUILD_NUMBER'
+git push origin testsresults/$TRAVIS_BUILD_NUMBER
